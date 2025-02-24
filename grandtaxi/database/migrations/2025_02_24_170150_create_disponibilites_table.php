0fe294c25981f43e1c05c4f76428f30dfd9b9f7b
FadwaElouah <fadwaelouah@gmail.com>
2025-02-24 18:56:21 +0000

    add modified uml

---
+++ grandtaxi/database/migrations/2025_02_24_170150_create_disponibilites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disponibilites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chauffeur_id')->constrained('users')->onDelete('cascade');
+            $table->datetime('date_debut');
+            $table->datetime('date_fin');
+            $table->enum('statut', ['disponible', 'occupé']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disponibilites');
+    }
+};
