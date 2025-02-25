ebadb5ae97d98c2657feff1eb759a2d2afc75a5c
FadwaElouah <fadwaelouah@gmail.com>
2025-02-25 11:52:03 +0000

    did fillale disponibilite

--- grandtaxi/app/Models/disponibilite.php
+++ grandtaxi/app/Models/disponibilite.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class disponibilite extends Model
 {
     use HasFactory;
+    protected $filable =[
+        'chauffeur_id',
+        'date_debut',
+        'date_fin',
+        'statut',
+    ];
 }
